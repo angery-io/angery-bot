ed8dfd4d3a9557c851f8fac15d49ac40e3d78b22
Peter Kovary <kovary.p23@gmail.com>
2019-02-23 15:03:18 +0000

    Add slash commands, users endpoint

--- angery.py
+++ angery.py
@@ -4,6 +4,7 @@ import sys
 import json
 from random import choice
 from urllib.request import urlopen, Request
+from urllib.parse import parse_qsl, urlencode
 import hmac
 import hashlib
 from time import time
@@ -12,6 +13,7 @@ import boto3
 
 SLACK_OAUTH_API = "https://slack.com/api/oauth.access"
 SLACK_POSTMESSAGE_API = "https://slack.com/api/chat.postMessage"
+SLACK_USER_API = "https://slack.com/api/users.info?"
 SLACK_TIMESTAMP = "X-Slack-Request-Timestamp"
 SLACK_SIGNATURE = "X-Slack-Signature"
 
@@ -28,6 +30,12 @@ ANGERY_TRIGGER = (
 ANGERY_RESOURCES = os.path.join("resources", "angery.txt")
 
 SDB_DOMAIN = "BOT_TOKENS"
+
+
+def get_sdb(singleton=[]):
+    if not singleton:
+        singleton.append(boto3.client("sdb", region_name="eu-west-1"))
+    return singleton[0]
 
 
 def verify_request_comes_from_slack(event):
@@ -53,19 +61,25 @@ def verify_request_comes_from_slack(event):
         raise RuntimeError("Slack signature not matching")
 
 
-def put_token(sdb, domain, team_id, token):
+def put_attributes(sdb, domain, team_id, token=None, reacts=True, **kwargs):
+    attributes = [
+        {"Name": name, "Value": value, "Replace": True}
+        for name, value in (
+            ([("token", token)] if token else [])
+            + ([("reacts", str(reacts))] if reacts else [])
+            + list(kwargs.items())
+        )
+    ]
     response = sdb.put_attributes(
-        DomainName=domain,
-        ItemName=team_id,
-        Attributes=[{"Name": "token", "Value": token, "Replace": True}],
+        DomainName=domain, ItemName=team_id, Attributes=attributes
     )
 
 
-def get_token(sdb, domain, team_id):
+def get_attributes(sdb, domain, team_id):
     response = sdb.get_attributes(
-        DomainName=domain, ItemName=team_id, AttributeNames=["token"]
+        DomainName=domain, ItemName=team_id, ConsistentRead=True
     )
-    return response.get("Attributes", [{}])[0].get("Value", None)
+    return {attr["Name"]: attr["Value"] for attr in response.get("Attributes", [])}
 
 
 def register(params):
@@ -90,26 +104,48 @@ def register(params):
         auth_details = json.load(response)
 
     team_id = auth_details.get("team_id")
+    team_name = auth_details.get("team_name")
     bot_token = auth_details.get("bot", {}).get("bot_access_token")
     if not auth_details.get("ok") or not team_id or not bot_token:
         raise RuntimeError("Error getting auth tokens")
 
-    sdb = boto3.client("sdb", region_name="eu-west-1")
-    put_token(sdb, SDB_DOMAIN, team_id, bot_token)
+    put_attributes(get_sdb(), SDB_DOMAIN, team_id, bot_token, team_name=team_name)
 
 
-def lambda_handler(event, context):
-    if event.get("resource") == "/AngeryBot/register":
-        register(event.get("queryStringParameters", {}))
-        return {
-            "body": "We will now deliver your angeries - don't forget to invite the Angery bot to your channels!"
-        }
+def check_admin(user_id, bot_token):
+    data = {"token": bot_token, "user": user_id}
+    request = Request(SLACK_USER_API + urlencode(data))
+    with urlopen(request) as response:
+        user = json.load(response)
+    if not user.get("ok"):
+        raise RuntimeError("Something wrong with user api")
+    return user["user"]["is_admin"]
 
-    verify_request_comes_from_slack(event)
 
-    body = json.loads(event["body"])
+def slash_command(body):
+    if body.get("command") == "/angery-reacts":
+        if body.get("text") not in ("activate", "deactivate"):
+            return "Not sure what you mean :angry:"
 
+        sdb = get_sdb()
+        team_id = body["team_id"]
+        attributes = get_attributes(sdb, SDB_DOMAIN, team_id)
+        if not check_admin(body["user_id"], attributes["token"]):
+            return "I don't talk to you non-admin folk"
+
+        if body["text"] == "deactivate":
+            put_attributes(sdb, SDB_DOMAIN, team_id, reacts=False)
+            return "Angery reacts deactivated"
+        else:
+            put_attributes(sdb, SDB_DOMAIN, team_id, reacts=True)
+            return "Angery reacts activated"
+    else:
+        return None
+
+
+def handle_message(body):
     if "challenge" in body:
+        print(event, context)
         return {"body": body["challenge"]}
 
     else:
@@ -131,8 +167,13 @@ def lambda_handler(event, context):
                 imgs = f.readlines()
             img = choice(imgs)[:-1]
 
-            sdb = boto3.client("sdb", region_name="eu-west-1")
-            token = get_token(sdb, SDB_DOMAIN, team_id)
+            sdb = get_sdb()
+            attributes = get_attributes(sdb, SDB_DOMAIN, team_id)
+            token = attributes.get("token")
+            if not token:
+                raise RuntimeError(f"No token for team_id: {team_id}")
+            if reaction and attributes.get("reacts", "True") != "True":
+                return {}
             payload = {
                 "channel": channel,
                 "attachments": [{"image_url": img, "fallback": "Angery react"}],
@@ -148,4 +189,39 @@ def lambda_handler(event, context):
                 method="POST",
             )
             urlopen(request)
-        return {}
+
+
+def lambda_handler(event, context):
+    if event.get("resource") == "/AngeryBot/register":
+        register(event.get("queryStringParameters", {}))
+        try:
+            SLACK_APP_ID = os.environ["SLACK_APP_ID"]
+            return {
+                "statusCode": 302,
+                "headers": {"Location": f"https://slack.com/apps/{SLACK_APP_ID}"},
+            }
+        except KeyError:
+            return {
+                "body": "We will now deliver your angeries - don't forget to invite the Angery bot to your channels!"
+            }
+
+    elif event.get("resource") == "/AngeryBot/users":
+        sdb = get_sdb()
+        items = sdb.select(SelectExpression=f"select team_name from {SDB_DOMAIN}").get(
+            "Items", []
+        )
+        response = {"users": [i["Attributes"][0]["Value"] for i in items]}
+        response["count"] = len(response["users"])
+        return {"body": json.dumps(response)}
+    elif event.get("resource") == "/AngeryBot/command":
+        verify_request_comes_from_slack(event)
+        response = slash_command(dict(parse_qsl(event["body"])))
+        return {"statusCode": 200 if response else 400, "body": response}
+
+    verify_request_comes_from_slack(event)
+    body = json.loads(event["body"])
+    if body.get("event", {}).get("type") == "app_uninstalled":
+        get_sdb().delete_attributes(DomainName=SDB_DOMAIN, ItemName=body["team_id"])
+    else:
+        handle_message(body)
+    return {}
